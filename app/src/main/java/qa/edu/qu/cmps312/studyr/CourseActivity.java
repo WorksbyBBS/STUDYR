1f42192094e194834240d0088e267b31faa684ed
Alo <bbscydia@gmail.com>
2018-11-14 01:00:25 +0300

    commit coursesActivity

---
+++ app/src/main/java/qa/edu/qu/cmps312/studyr/CourseActivity.java
@@ -0,0 +1,13 @@
+package qa.edu.qu.cmps312.studyr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CourseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_course);
+    }
+}

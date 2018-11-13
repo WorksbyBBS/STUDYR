31a747e9a45c6b03a0ddf464c95c6aeaaff97b33
Alo <bbscydia@gmail.com>
2018-11-14 00:54:58 +0300

    commit

---
+++ app/src/main/java/qa/edu/qu/cmps312/studyr/AssignmentsActivity.java
@@ -0,0 +1,13 @@
+package qa.edu.qu.cmps312.studyr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AssignmentsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_assignments);
+    }
+}
